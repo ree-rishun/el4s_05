d81767fe481bc2b89b4dcd4500fa8d82594731fd
ree-rishun <ree0432@gmail.com>
2020-11-14 13:28:02 +0900

    genre返却APIの実装完了

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 from flask import Flask
-from firestoreDb import db
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
 
+cred = credentials.Certificate("el4s-05-firebase-adminsdk-988jv-eaa46c25d2.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
+
+# ジャンル一覧取得
 @app.route('/genre')
 def genre():
-    genres = db.collection('genres').get()
-    return genres
+    # データ取得
+    genres = db.collection(u'genres').get()
+    json = '{'
+
+    # データの整形
+    for doc in genres:
+        # カンマ区切りの挿入
+        if json != '{':
+            json += ','
+
+        # JSON形式に整形
+        json += u'\"' + doc.id + '\":{\"name\": \" ' + doc.to_dict()['name'] + ' \"}'
+
+    json += '}'
+
+    # JSONの返却
+    return json
 
 
+# 音楽取得
 @app.route('/music')
 def music_select():
     return 'test'
--- firestoreDb.py
+++ firestoreDb.py
@@ -3,6 +3,6 @@ from firebase_admin import credentials, firestore
 
 
 def db():
-    cred = credentials.Certificate("path/to/serviceAccountKey.json")
+    cred = credentials.Certificate("el4s-05-firebase-adminsdk-988jv-eaa46c25d2.json")
     firebase_admin.initialize_app(cred)
     return firestore.client()
